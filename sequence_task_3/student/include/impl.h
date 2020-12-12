ee3f07ae16b7b0613f482b504082f6b9040f35de
Novikov <INovikov@kiev.luxoft.com>
2020-12-12 21:16:31 +0200

    Implementation of sequence_task_3 by Novikov

--- sequence_task_3/student/include/impl.h
+++ sequence_task_3/student/include/impl.h
@@ -1,4 +1,5 @@
 #pragma once
+#include <iostream>
 #include <sstream>
 #include <deque>
 #include <iterator>
@@ -11,4 +12,15 @@
  *
  * @warning YOU MUST USE STRING STREAMS AND STREAM ITERATORS!
  */
-std::string reverseWords(const std::string& input);
+std::string reverseWords(const std::string& input) {
+    std::istringstream iss(input);
+    std::ostringstream oss;
+    std::deque<std::string> wordCollector;
+
+    std::copy(std::istream_iterator<std::string>(iss), std::istream_iterator<std::string>(), std::front_inserter(wordCollector));
+    std::copy(wordCollector.begin(), --wordCollector.end(), std::ostream_iterator<std::string>(oss, " "));
+    if(!wordCollector.empty()) {
+        oss << wordCollector.back();
+    }
+    return oss.str();
+}
